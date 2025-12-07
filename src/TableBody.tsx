e21d6b9df2b167fc906160b8b05d751ac02b6940
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-12-07 19:30:44 +0100

    style: refactor TodoRow component to use defined props type for improved clarity and organization

--- src/TableBody.tsx
+++ src/TableBody.tsx
@@ -5,7 +5,10 @@ import EditableTextCell from './EditableTextCell.tsx'
 import DueCell from './DueCell.tsx'
 import StateCell from './StateCell.tsx'
 
-
+type TodoRowProps = {
+    item: TodoItem;
+    onUpdateTodo: (id: string, patch: Partial<TodoItem>) => void;
+}
 const Priority = (value: Value, urgency: Value): number => {
     const valueNumeric = Number(value);
     const urgencyNumeric = Number(urgency);
@@ -13,13 +16,7 @@ const Priority = (value: Value, urgency: Value): number => {
     return Math.floor((valueNumeric * 2 + urgencyNumeric) / 15 * 100)
 }
 
-function TodoRow({
-    item,
-    onUpdateTodo,
-}: {
-    item: TodoItem;
-    onUpdateTodo: (id: string, patch: Partial<TodoItem>) => void;
-}) {
+function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
     return (
         <tr style={{ borderBottom: '1px solid #ccc' }}>
             <td>
@@ -35,21 +32,18 @@ function TodoRow({
                 />
             </td>
             <td>{Priority(item.value, item.urgency)}%</td>
-
             <td>
                 <ValueCell
                     value={item.value}
                     onChange={(newValue) => onUpdateTodo(item.id, { value: newValue })}
                 />
             </td>
-
             <td>
                 <ValueCell
                     value={item.urgency}
                     onChange={(newUrgency) => onUpdateTodo(item.id, { urgency: newUrgency })}
                 />
             </td>
-
             <td>
                 <EffortCell
                     effort={item.effort}
@@ -64,25 +58,21 @@ function TodoRow({
             </td>
             <td>
                 <EditableTextCell
-                    value={item.collaboration ? item.collaboration : ''}
-                    onChange={(newName: string) => onUpdateTodo(item.id, { collaboration: newName })}
+                    value={item.collaboration ??''}
+                    onChange={(newName: string) => onUpdateTodo(item.id, { collaboration: newName || undefined })}
                 />
-
             </td>
-
             <td>
                 <DueCell
                     date={item.duedate ? item.duedate : ''}
                     onChange={(newDate: string) => onUpdateTodo(item.id, { duedate: newDate })}
                 ></DueCell>
             </td>
-
             <td>
                 <EditableTextCell
                     value={item.notes ? item.notes : ''}
                     onChange={(newName: string) => onUpdateTodo(item.id, { notes: newName })}
                 />
-
             </td>
         </tr>
     );
