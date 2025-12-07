5ca2717ba3b57d4f03fe0c08fd51a58435401f87
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-12-07 19:28:00 +0100

    style: refactor TodoTable component for improved readability and organization

--- src/TodoTable.tsx
+++ src/TodoTable.tsx
@@ -1,39 +1,34 @@
-import {useState, useMemo} from 'react'
+import { useState, useMemo } from 'react'
 import TableHeader from './TableHeader.tsx'
 import TableBody from './TableBody.tsx'
 
-
 import type { TodoItem } from './types.ts'
-
-
-
-
-function TodoTable({
-    todos,
-    onUpdateTodo,
-}: {
+type TodoTableProps = {
     todos: TodoItem[];
     onUpdateTodo: (id: string, patch: Partial<TodoItem>) => void;
-}) {
+}
+
+function compareByValueThenUrgency(a: TodoItem, b: TodoItem, direction: 'asc' | 'desc') {
+  const va = Number(a.value)
+  const vb = Number(b.value)
+  if (va !== vb) return direction === 'asc' ? va - vb : vb - va
+
+  const ua = Number(a.urgency)
+  const ub = Number(b.urgency)
+  return direction === 'asc' ? ua - ub : ub - ua
+}
+
+function TodoTable({ todos, onUpdateTodo }: TodoTableProps) {
     const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc');
 
     const sortedTodos = useMemo(() => {
         const copy = [...todos];
-        copy.sort((a, b) => {
-            const va = Number(a.value);
-            const vb = Number(b.value);
-            if (va !== vb) {
-                return sortDirection === 'asc' ? va - vb : vb - va;
-            }
-            const ua = Number(a.urgency);
-            const ub = Number(b.urgency);
-            return sortDirection === 'asc' ? ua - ub : ub - ua;
-        });
+        copy.sort((a, b) => compareByValueThenUrgency(a, b, sortDirection));
         return copy;
     }, [todos, sortDirection]);
 
     return (
-        <table style ={{ borderCollapse: 'collapse', width: '100%' }}>
+        <table style={{ borderCollapse: 'collapse', width: '100%' }}>
             <TableHeader sortDirection={sortDirection} onToggleSort={() => setSortDirection((s) => s === 'asc' ? 'desc' : 'asc')} />
             <TableBody todos={sortedTodos} onUpdateTodo={onUpdateTodo} />
         </table>
