ad6ac303e1e1cf4209851d5a7df9dbfe1e9084f2
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-12-08 22:01:02 +0100

    better formula

--- src/TableBody.tsx
+++ src/TableBody.tsx
@@ -9,13 +9,15 @@ type TodoRowProps = {
     item: TodoItem;
     onUpdateTodo: (id: string, patch: Partial<TodoItem>) => void;
 }
-const Priority = (value: Value, duedate: string, effort: Effort): number => {
+
+const DEFAULT_TIME_PRESSURE = 180
+
+const priority = (value: Value, duedate: string, effort: Effort): number => {
     const valueNumeric = Number(value);
-    const urgencyNumeric = Urgency(duedate, effort); // Highest urgency is loest number (and negatives are overdue)
-    console.log("Calculating priority for value:", valueNumeric, "and urgency:", urgencyNumeric);
-    return valueNumeric * -urgencyNumeric;
+    const urgencyNumeric = urgency(duedate, effort); // Highest urgency is loest number (and negatives are overdue)
+    return urgencyNumeric / valueNumeric;
 }
-const DaysLeft = (duedate: string) => { // shitty algorithm
+const daysLeft = (duedate: string) => { // shitty algorithm
     const convertedTimestamp = new Date(duedate).getTime();
     console.log("Converted timestamp for duedate", duedate, "is", convertedTimestamp);
     if (isNaN(convertedTimestamp)) {
@@ -29,9 +31,9 @@ const DaysLeft = (duedate: string) => { // shitty algorithm
     const differenceInDays = Math.ceil(differenceInMilliseconds / millisecondsInADay);
 
     return differenceInDays;
-}   
+}
 
-const EffortInDays = (effort: Effort) => {
+const effortInDays = (effort: Effort) => {
     switch (effort) {
         case 'MINUTES':
             return 1;
@@ -40,22 +42,25 @@ const EffortInDays = (effort: Effort) => {
         case 'DAYS':
             return 3;
         case 'WEEKS':
-            return 3*7;
+            return 21;
         case 'MONTHS':
-            return 3*30;
+            return 90;
         default:
             return 3;
     }
 }
 
-const Urgency = (duedate: string, effort: Effort): number => {
-    const daysLeft = DaysLeft(duedate);
-    if (daysLeft === null) {
-        return 180; // Default urgency for invalid dates
+
+const urgency = (duedate: string, effort: Effort): number => {
+    const left = daysLeft(duedate);
+
+    const needed = effortInDays(effort);
+
+    if (left === null) {
+        return DEFAULT_TIME_PRESSURE - needed;
+    } else {
+        return left - needed;
     }
-    const effortInDays = EffortInDays(effort);
-    console.log("Calculating urgency for days left:", daysLeft, "and effort in days:", effortInDays);
-    return daysLeft - effortInDays;
 }
 
 function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
@@ -73,7 +78,7 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
                     onChange={(newState) => onUpdateTodo(item.id, { state: newState })}
                 />
             </td>
-            <td>{Priority(item.value, item.duedate ? item.duedate : '', item.effort)}</td>
+            <td>{priority(item.value, item.duedate ? item.duedate : '', item.effort)}</td>
             <td>
                 <ValueCell
                     value={item.value}
@@ -81,7 +86,7 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
                 />
             </td>
             <td>
-                { Urgency(item.duedate ? item.duedate : '', item.effort) }
+                {urgency(item.duedate ? item.duedate : '', item.effort)}
             </td>
             <td>
                 <EffortCell
@@ -97,7 +102,7 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
             </td>
             <td>
                 <EditableTextCell
-                    value={item.collaboration ??''}
+                    value={item.collaboration ?? ''}
                     onChange={(newName: string) => onUpdateTodo(item.id, { collaboration: newName || undefined })}
                 />
             </td>
