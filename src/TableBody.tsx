01b8ac42164c6cc02af156fa0c66f664ef0a3911
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-12-13 18:40:35 +0100

    prepare dependency feature

--- src/TableBody.tsx
+++ src/TableBody.tsx
@@ -1,18 +1,36 @@
 import { useState } from 'react'
 import type { TodoItem } from './types.ts'
+
 import ValueCell from './ValueCell.tsx'
 import EffortCell from './EffortCell.tsx'
 import EditableTextCell from './EditableTextCell.tsx'
 import DueCell from './DueCell.tsx'
 import StateCell from './StateCell.tsx'
+
 import { calcDaysLeft, calcRatio, calcUrgency, urgencyLabels, priority } from './utils.ts'
+
+type PlusButtonProps = {
+    show: Boolean;
+    onClick: () => void;
+};
+
+function PlusButton({ show, onClick }: PlusButtonProps) {
+    const style = {
+        backgroundColor: 'Canvas',
+        border: 'none',
+    }
+
+    return (
+        <button type="button" style={style} title="Add Subtask" onClick={onClick}>
+            {show ? '➖' : '➕'}
+        </button>
+    );
+}
 
 type TodoRowProps = {
     item: TodoItem;
     onUpdateTodo: (id: string, patch: Partial<TodoItem>) => void;
 }
-
-
 
 function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
     const daysLeft = calcDaysLeft(item.duedate);
@@ -20,10 +38,12 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
     const urgency = calcUrgency(ratio);
 
     const [showNoteEditor, setShowNoteEditor] = useState(false);
-
     function handleClickOnNote() {
         showNoteEditor ? setShowNoteEditor(false) : setShowNoteEditor(true);
     }
+
+    const [showDependencies, setShowDependencies] = useState(false);
+    const [showAddDependencyInput, setShowAddDependencyInput] = useState(false);
 
     if (showNoteEditor) {
         return (
@@ -31,7 +51,7 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
                 <td colSpan={10}>
                     <h1 style={{ marginBottom: '0.5em' }}>Edit Notes</h1>
                     <h2>Task: {item.task}</h2>
-             
+
                     <textarea
                         autoFocus
                         style={{ width: '100%', height: '100px' }}
@@ -43,70 +63,98 @@ function TodoRow({ item, onUpdateTodo }: TodoRowProps) {
             </tr>
         );
     }
-        
+
     return (
-        <tr>
-            <td>
-                <EditableTextCell
-                    value={item.task}
-                    onChange={(newName: string) => onUpdateTodo(item.id, { task: newName })}
-                />
-            </td>
-            <td>
-                <StateCell
-                    state={item.state}
-                    onChange={(newState) => onUpdateTodo(item.id, { state: newState })}
-                />
-            </td>
-            <td>
-                <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText' }}>
-                    {priority(item.value, urgency) * 4}%
-                </span>
-            </td>
-            <td>
-                <ValueCell
-                    value={item.value}
-                    onChange={(newValue) => onUpdateTodo(item.id, { value: newValue })}
-                />
-            </td>
-            <td>
-                <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText' }}>
-                    {urgencyLabels[urgency]}
-                </span>
-            </td>
-            <td>
-                <EffortCell
-                    effort={item.effort}
-                    onChange={(newEffort) => onUpdateTodo(item.id, { effort: newEffort })}
-                />
-            </td>
-            <td>
-                <EditableTextCell
-                    value={item.responsible}
-                    onChange={(newName: string) => onUpdateTodo(item.id, { responsible: newName })}
-                />
-            </td>
-            <td>
-                <EditableTextCell
-                    value={item.collaboration ?? ''}
-                    onChange={(newName: string) => onUpdateTodo(item.id, { collaboration: newName || undefined })}
-                />
-            </td>
-            <td>
-                <DueCell
-                    date={item.duedate ? item.duedate : ''}
-                    onChange={(newDate: string) => onUpdateTodo(item.id, { duedate: newDate })}
-                ></DueCell>
-                <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText', marginLeft: '1ch' }}>
-                    {daysLeft !== null ? ` ${daysLeft} days left` : ''}
-                </span>
-            </td>
-            <td>
-                <div onClick={handleClickOnNote} style={{ cursor: 'pointer', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', maxWidth: '200px' }}>
-                    {item.notes ? item.notes : <span style={{ fontStyle: 'italic', color: 'gray' }}>Add notes...</span>}
-                </div> 
-            </td>
-        </tr>
+        <>
+            <tr>
+                <td>
+                    <EditableTextCell
+                        value={item.task}
+                        onChange={(newName: string) => onUpdateTodo(item.id, { task: newName })}
+                    />
+                    <PlusButton show={showDependencies} onClick={() => setShowDependencies((v) => !v)} />
+                </td>
+                <td>
+                    <StateCell
+                        state={item.state}
+                        onChange={(newState) => onUpdateTodo(item.id, { state: newState })}
+                    />
+                </td>
+                <td>
+                    <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText' }}>
+                        {priority(item.value, urgency) * 4}%
+                    </span>
+                </td>
+                <td>
+                    <ValueCell
+                        value={item.value}
+                        onChange={(newValue) => onUpdateTodo(item.id, { value: newValue })}
+                    />
+                </td>
+                <td>
+                    <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText' }}>
+                        {urgencyLabels[urgency]}
+                    </span>
+                </td>
+                <td>
+                    <EffortCell
+                        effort={item.effort}
+                        onChange={(newEffort) => onUpdateTodo(item.id, { effort: newEffort })}
+                    />
+                </td>
+                <td>
+                    <EditableTextCell
+                        value={item.responsible}
+                        onChange={(newName: string) => onUpdateTodo(item.id, { responsible: newName })}
+                    />
+                </td>
+                <td>
+                    <EditableTextCell
+                        value={item.collaboration ?? ''}
+                        onChange={(newName: string) => onUpdateTodo(item.id, { collaboration: newName || undefined })}
+                    />
+                </td>
+                <td>
+                    <DueCell
+                        date={item.duedate ? item.duedate : ''}
+                        onChange={(newDate: string) => onUpdateTodo(item.id, { duedate: newDate })}
+                    ></DueCell>
+                    <span style={{ fontWeight: item.state === 'DONE' ? 'lighter' : 'bold', color: item.state === 'DONE' ? 'gray' : 'CanvasText', marginLeft: '1ch' }}>
+                        {daysLeft !== null ? ` ${daysLeft} days left` : ''}
+                    </span>
+                </td>
+                <td>
+                    <div onClick={handleClickOnNote} style={{ cursor: 'pointer', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap', maxWidth: '200px' }}>
+                        {item.notes ? item.notes : <span style={{ fontStyle: 'italic', color: 'gray' }}>Add notes...</span>}
+                    </div>
+                </td>
+            </tr>
+            {showDependencies && (
+                <>
+                    <tr>
+                        <td colSpan={10}>
+                            <h2>Dependencies</h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={10}>
+                            {showAddDependencyInput ? (
+                                <div>
+                                    <input type="text" autoFocus />
+                                    { // I AM FUMBLING OVER THIS
+                                    }
+                                </div>
+                            ) : (
+                                <button className="primary" onClick={() => setShowAddDependencyInput((v) => !v)}>
+                                    + Add Dependency
+                                </button>
+                            )}
+                        </td>
+                    </tr>
+                </>
+            )}
+
+        </>
     );
 }
 
